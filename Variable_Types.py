7bfaa7b929e07e663960b1a1b2b3ef35a995dece
Nerdboy_Q <princtonb@gmail.com>
2019-06-05 16:58:29 -0700

    Create Variable_Types.py

---
+++ Variable_Types.py
@@ -0,0 +1,24 @@
+'''Basic Intro to Classes/Variable Types'''
+
+#collections
+list_var = ["apples","oranges","bananas","pears"]
+dictionary_var = {
+"name" : "",
+"age" : "",
+"city" : "",
+}
+tuple_var = ("fruits","veggies","dairy")
+#Strings
+basic_string_var = "basic"
+long_string_var = "this is my string"
+
+#numbers
+integer_var = 5
+float_var = 3.14
+floar_sci_var = 305e-6
+complex_var = 3.11j
+
+#Computer based values
+boolean_var = False
+hex_var = 0xAF
+bin_var = 0b1111
